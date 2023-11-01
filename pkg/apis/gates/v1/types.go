4b200cc916c65deec0544a99d6bcd0b44c39d995
jrodonnell <james.riley.odonnell@gmail.com>
2023-10-31 19:56:35 -0600

    fix yaml unmarshaling

--- pkg/apis/gates/v1/types.go
+++ pkg/apis/gates/v1/types.go
@@ -7,10 +7,6 @@ type Password struct {
 	Length uint8 `json:"length,omitempty"`
 }
 
-type Passwords struct {
-	Passwords []Password `json:"passwords"`
-}
-
 type PGPKey struct {
 	ID      `json:"id,omitempty"`
 	Name    string `json:"name,omitempty"`
@@ -18,23 +14,11 @@ type PGPKey struct {
 	Email   string `json:"email,omitempty"`
 }
 
-type PGPKeys struct {
-	PGPKeys []PGPKey `json:"pgpKeys"`
-}
-
 type SSHKey struct {
 	ID `json:"id,omitempty"`
-}
-
-type SSHKeys struct {
-	SSHKeys []SSHKey `json:"sshKeys"`
 }
 
 type TLSCert struct {
 	ID       `json:"id,omitempty"`
 	Hostname string `json:"hostname,omitempty"`
-}
-
-type TLSCerts struct {
-	TLSCerts []TLSCert `json:"tlsCerts"`
 }
--- pkg/config/parse.go
+++ pkg/config/parse.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/mitchellh/mapstructure"
 	gatesv1 "github.com/the-gizmo-dojo/g8/pkg/apis/gates/v1"
 	yaml "gopkg.in/yaml.v3"
 )
@@ -12,75 +11,26 @@ import (
 type ConfigFile string
 
 const (
-	DefaultFile    ConfigFile = "gates.yaml"
-	passwordsField string     = "passwords"
-	pgpKeysField   string     = "pgpKeys"
-	sshKeysField   string     = "sshKeys"
-	tlsCertsField  string     = "tlsCerts"
+	DefaultFile ConfigFile = "gates.yaml"
 )
 
 type Config struct {
-	gatesv1.Passwords
-	gatesv1.PGPKeys
-	gatesv1.SSHKeys
-	gatesv1.TLSCerts
+	Passwords []gatesv1.Password `json:"passwords,omitempty" yaml:"passwords,omitempty"`
+	PGPKeys   []gatesv1.PGPKey   `json:"pgpKeys,omitempty"   yaml:"pgpKeys,omitempty"`
+	SSHKeys   []gatesv1.SSHKey   `json:"sshKeys,omitempty"   yaml:"sshKeys,omitempty"`
+	TLSCerts  []gatesv1.TLSCert  `json:"tlsCerts,omitempty"  yaml:"tlsCerts,omitempty"`
 }
 
 func Parse(cf ConfigFile) (Config, error) {
-	yml := make(map[string]interface{})
+	var config Config
 	contents, err := os.ReadFile(string(cf))
 	if err != nil {
 		err = fmt.Errorf("Error reading %s file", cf)
 	}
 
-	err = yaml.Unmarshal(contents, yml)
+	err = yaml.Unmarshal(contents, &config)
 	if err != nil {
 		err = fmt.Errorf("Error unmarshaling %s file", cf)
-	}
-
-	var config Config
-	var passwords gatesv1.Passwords
-	var pgpkeys gatesv1.PGPKeys
-	var sshkeys gatesv1.SSHKeys
-	var tlscerts gatesv1.TLSCerts
-	for k, v := range yml {
-		switch k {
-		case passwordsField:
-			for _, m := range v.([]interface{}) {
-				var password gatesv1.Password
-				err = mapstructure.Decode(m, &password)
-				passwords.Passwords = append(passwords.Passwords, password)
-			}
-		case pgpKeysField:
-			for _, m := range v.([]interface{}) {
-				var pgpkey gatesv1.PGPKey
-				err = mapstructure.Decode(m, &pgpkey)
-				pgpkeys.PGPKeys = append(pgpkeys.PGPKeys, pgpkey)
-			}
-		case sshKeysField:
-			for _, m := range v.([]interface{}) {
-				var sshkey gatesv1.SSHKey
-				err = mapstructure.Decode(m, &sshkey)
-				sshkeys.SSHKeys = append(sshkeys.SSHKeys, sshkey)
-			}
-		case tlsCertsField:
-			for _, m := range v.([]interface{}) {
-				var tlscert gatesv1.TLSCert
-				err = mapstructure.Decode(m, &tlscert)
-				tlscerts.TLSCerts = append(tlscerts.TLSCerts, tlscert)
-			}
-		default:
-			err = fmt.Errorf("Unknown field in %s file!", cf)
-			fmt.Println(err)
-			os.Exit(1)
-		}
-	}
-
-	config = Config{
-		Passwords: passwords,
-		PGPKeys:   pgpkeys,
-		SSHKeys:   sshkeys,
-		TLSCerts:  tlscerts,
 	}
 
 	return config, err
