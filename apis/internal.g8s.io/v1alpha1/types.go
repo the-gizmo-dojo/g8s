490be596a24c265471c4b26b054b052c3e292b38
jrodonnell <james.riley.odonnell@gmail.com>
2023-11-21 19:01:09 -0700

    building out core API, using qanda

--- apis/internal.g8s.io/v1alpha1/types.go
+++ apis/internal.g8s.io/v1alpha1/types.go
@@ -1,26 +1,52 @@
 package v1alpha1
 
-type Source string
+type Name string
 
-const (
-	Generated Source = "generated"
-	Input     Source = "input"
-)
-
-type Generator struct {
-	Spec GeneratorSpec
+type Generator[G any] struct {
+	GeneratorFunc G
+	Options
 }
 
-type GeneratorSpec struct {
-	GenerateFunc func(any) any
-	Options      []Option
+type Options interface {
+	Validate() error
 }
 
-type Option struct {
+type NamespaceOptions struct {
+	Name `json:"name,omitempty"`
+}
+
+func (nso NamespaceOptions) Validate() error {
+	// TODO for now just getting the basics working
+	return nil
 }
 
 type PasswordOptions struct {
+	Name   `json:"name,omitempty"`
+	Length uint8 `json:"length,omitempty"`
 }
 
-type PasswordGenerator struct {
+func (po PasswordOptions) Validate() error {
+	// TODO for now just getting the basics working
+	return nil
+}
+
+type PGPKeyOptions struct {
+	Name       `json:"name,omitempty"`
+	EntityName string `json:"entityName,omitempty"`
+	Address    string `json:"address,omitempty"`
+	Email      string `json:"email,omitempty"`
+}
+
+func (pgpo PGPKeyOptions) Validate() error {
+	// TODO for now just getting the basics working
+	return nil
+}
+
+type SSHKeyOptions struct {
+	Name `json:"name,omitempty"`
+}
+
+func (ssho SSHKeyOptions) Validate() error {
+	// TODO for now just getting the basics working
+	return nil
 }
